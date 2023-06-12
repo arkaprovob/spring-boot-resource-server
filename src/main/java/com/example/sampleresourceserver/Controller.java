593888240be90243cb55e58e93025c5cf8137f8f
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-06-12 19:04:37 +0530

    initial commit

---
+++ src/main/java/com/example/sampleresourceserver/Controller.java
@@ -0,0 +1,22 @@
+package com.example.sampleresourceserver;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/resource")
+    public ResponseEntity<String> secret(@RequestHeader Map<String, String> headers) {
+        headers.forEach((key, value) -> {
+            System.out.println(String.format("Header '%s' = %s", key, value));
+        });
+        return new ResponseEntity<>("Treasure!!!!!!", HttpStatus.OK);
+    }
+
+}
